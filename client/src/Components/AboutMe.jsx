7f206882ae07d00b7c7303ae78cd25715fe463b6
Devon <devonsmith91@gmail.com>
2021-04-17 15:24:26 -0400

    about page completed

--- client/src/Components/AboutMe.jsx
+++ client/src/Components/AboutMe.jsx
@@ -30,40 +30,60 @@ export default function AboutMe() {
             <button className="frontFlipButton" onClick={handleClick}>
               About My Life
             </button>
-						<div className="languageBlock">Languages:</div>
-						<div className="frameworkBlock">Frameworks:</div>
-						<div className="databaseBlock">Databases:</div>
-						<div className="applicationsBlock">Applications:</div>
+            <div className="languageBlock">Languages:</div>
+            <div className="frameworkBlock">Frameworks:</div>
+            <div className="databaseBlock">Databases:</div>
+            <div className="applicationsBlock">Applications:</div>
             <div className="frontCard">
               <ul className="languageUl">
                 <li>JavaScript</li>
                 <li>HTML5</li>
                 <li>CSS3</li>
                 <li>Node</li>
-							</ul>
-							<ul className="frameworkUl">
-								<li>React</li>
-								<li>Express JS</li>
-								<li>Vue JS</li>
-							</ul>
-						</div>
-						<div className="frontInnerCard">
-							<ul className="databaseUl">
-								<li>MongoDB</li>
-								<li>FireBase</li>
-							</ul>
-							<ul className="applicationsUl">
-								<li>Visual Studio Code</li>
-								<li>GitHub</li>
-							</ul>
-						</div>
+              </ul>
+              <ul className="frameworkUl">
+                <li>React</li>
+                <li>Express JS</li>
+                <li>Vue JS</li>
+              </ul>
+            </div>
+            <div className="frontInnerCard">
+              <ul className="databaseUl">
+                <li>MongoDB</li>
+                <li>FireBase</li>
+              </ul>
+              <ul className="applicationsUl">
+                <li>Visual Studio Code</li>
+                <li>GitHub</li>
+              </ul>
+            </div>
           </div>
           <div className="backCardWrapper">
             <button className="backFlipButton" onClick={handleClick}>
               About My Work
             </button>
             <div className="backCard">
-              This is the back of the ReactCardFlip
+              <div className="sportsBlock">Sports:</div>
+              <div className="gamesBlock">Games:</div>
+              <div className="hobbiesBlock">Hobbies:</div>
+              <ul className="sportsUl">
+                <li>Snowboarding</li>
+                <li>Mountain Biking</li>
+                <li>Soccer</li>
+              </ul>
+              <ul className="hobbiesUl">
+                <li>Glass Blowing</li>
+                <li>Art Projects</li>
+                <li>Cooking</li>
+              </ul>
+              <div className="backInnerCard">
+                <ul className="gamesUl">
+                  <li>Divinity</li>
+                  <li>Diablo 2</li>
+                  <li>Borderlands</li>
+                  <li>League of Legends</li>
+                </ul>
+              </div>
             </div>
           </div>
         </ReactCardFlip>
